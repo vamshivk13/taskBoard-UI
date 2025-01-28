a526bdc00f48052a0b58a8a2eaa9bb43d54e3541
thatikondavamshi13 <thatikondavamshi13@gmail.com>
2025-01-28 10:33:49 +0530

    dev prod vite profiles added

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://task-board-backend-cbnz.onrender.com";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 // export const BASE_URL = "http://localhost:3000";
 
 //USER AUTH
